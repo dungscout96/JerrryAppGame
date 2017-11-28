e8254b20a180eb08d22fe2d47a25b961308ffd23
parker <p.sorenson@me.com>
2017-11-28 12:08:47 -0500

    For future reference: pictures and files that we will use in our project must not start with an upper-case letter

---
+++ app/src/main/java/edu/gettysburg/jerry_s_game/BalloonView.java
@@ -0,0 +1,30 @@
+package edu.gettysburg.jerry_s_game;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by parker on 11/28/17.
+ */
+
+public class BalloonView extends View {
+
+
+    public BalloonView(Context context) {
+        super(context);
+    }
+
+    public BalloonView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BalloonView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BalloonView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
