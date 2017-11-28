fa4ebcc3631abda2f9715322f300190f8d3719d8
parker <p.sorenson@me.com>
2017-11-28 12:39:08 -0500

    Started the basic layout of the game.

--- app/src/main/java/edu/gettysburg/jerry_s_game/BalloonView.java
+++ app/src/main/java/edu/gettysburg/jerry_s_game/BalloonView.java
@@ -1,8 +1,18 @@
 package edu.gettysburg.jerry_s_game;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.support.annotation.Nullable;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -11,20 +21,57 @@ import android.view.View;
 
 public class BalloonView extends View {
 
+    //fields
 
-    public BalloonView(Context context) {
-        super(context);
+    boolean isGameOver = false;
+    
+    static final Paint whitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+
+    public final Drawable[] balloons = new Drawable[8];
+    public final Bitmap[] bitmaps = new Bitmap[8];
+
+    //order of difficulty:
+    //black, purple, blue, green, yellow, orange, red
+    public static final Integer[] imageResIds = new Integer[]{0, R.drawable.black,
+            R.drawable.purple,R.drawable.blue,R.drawable.green, R.drawable.yellow,
+            R.drawable.orange,R.drawable.red,R.drawable.death,};
+
+    Resources res = null;
+
+
+    static{
+        whitePaint.setColor(Color.WHITE);
+        whitePaint.setStyle(Paint.Style.FILL);
     }
 
-    public BalloonView(Context context, @Nullable AttributeSet attrs) {
+
+    public BalloonView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        res = context.getResources();
+        init();
     }
 
-    public BalloonView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+
+    private void init(){
+        isGameOver = false;
+
     }
 
-    public BalloonView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        int width = getWidth();
+        int height = getHeight();
+
+
+
+        canvas.drawRect(0,0,width,height, whitePaint);
+
+
     }
+
+
+
 }
--- app/src/main/java/edu/gettysburg/jerry_s_game/MainActivity.java
+++ app/src/main/java/edu/gettysburg/jerry_s_game/MainActivity.java
@@ -1,10 +1,7 @@
 package edu.gettysburg.jerry_s_game;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,11 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -39,5 +34,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
