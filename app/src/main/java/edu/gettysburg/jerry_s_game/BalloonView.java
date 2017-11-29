3b02a0e9bd0e07c84debddeb4b969bd9d87a7a77
parker <p.sorenson@me.com>
2017-11-28 19:28:17 -0500

    Started the basic layout of the game.

--- app/src/main/java/edu/gettysburg/jerry_s_game/BalloonView.java
+++ app/src/main/java/edu/gettysburg/jerry_s_game/BalloonView.java
@@ -24,24 +24,30 @@ public class BalloonView extends View {
     //fields
 
     boolean isGameOver = false;
-    
-    static final Paint whitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+    static final Paint whitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    static final Paint bluePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+
+    static final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public final Drawable[] balloons = new Drawable[8];
     public final Bitmap[] bitmaps = new Bitmap[8];
+
+    Rect srcRect;
+    Rect destRect;
 
     //order of difficulty:
     //black, purple, blue, green, yellow, orange, red
     public static final Integer[] imageResIds = new Integer[]{0, R.drawable.black,
             R.drawable.purple,R.drawable.blue,R.drawable.green, R.drawable.yellow,
-            R.drawable.orange,R.drawable.red,R.drawable.death,};
+            R.drawable.orange,R.drawable.red};
 
     Resources res = null;
 
-
     static{
         whitePaint.setColor(Color.WHITE);
+        bluePaint.setColor(Color.BLUE);
         whitePaint.setStyle(Paint.Style.FILL);
     }
 
@@ -56,6 +62,13 @@ public class BalloonView extends View {
     private void init(){
         isGameOver = false;
 
+        for (int i = 1; i < imageResIds.length; i++){
+            balloons[i] = res.getDrawable(imageResIds[i]);
+            bitmaps[i] = BitmapFactory.decodeResource(res, imageResIds[i]);
+        }
+
+        srcRect = new Rect(0, 0, bitmaps[1].getWidth() - 1, bitmaps[1].getHeight() - 1);
+        destRect = new Rect(0, 0, bitmaps[1].getWidth() - 1, bitmaps[1].getHeight() - 1);
     }
 
     @Override
@@ -65,9 +78,9 @@ public class BalloonView extends View {
         int width = getWidth();
         int height = getHeight();
 
+        canvas.drawBitmap(bitmaps[2],srcRect, destRect,paint);
 
-
-        canvas.drawRect(0,0,width,height, whitePaint);
+        canvas.drawRect(0,0,width,height, bluePaint);
 
 
     }
